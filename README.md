fe2027dfafd6fed72ddfbc3706625792ec10585a
KentZhang <tyzhang108@gmail.com>
2023-08-12 21:15:18 -0700

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # codelink
+
+This is a small tool use to register a link url to system. 
+
+So I can open source file and locate to specific line in one click.
+
+It comes with a well configured Ini file.
+
+For now, it only support windows.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use ini::Ini;
+
+struct ToolChain {
+    name: String,
+    path: String,
+    cmd: String,
+}
+
+impl ToolChain {
+    fn new(name: String, path: String, cmd: String) -> ToolChain {
+        ToolChain {
+            name,
+            path,
+            cmd,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conf = Ini::load_from_file("config.ini").unwrap();
+
+    let current_tool = conf.section(Some("Current Tool")).unwrap();
+    let tool_name = current_tool.get("name").unwrap();
+
+    let mut tool_chains: Vec<ToolChain> = Vec::new();
+
+    // get all tool chain
+    for (sec, prop) in &conf {
+        if sec != "Current Tool" {
+            tool_chains.push(ToolChain::new(
+                sec.clone().unwrap(),
+                prop.get("path").unwrap().clone(),
+                prop.get("cmd").unwrap().clone(),
+            ));
+        }
+    }
 }
