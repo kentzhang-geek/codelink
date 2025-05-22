39154489003a18cc7a3cb6434eac203eca125434
Kent <tyzhang108@gmail.com>
2025-05-22 13:35:13 -0700

    parse link format

--- src/gen_by_file_line.rs
+++ src/gen_by_file_line.rs
@@ -1,7 +1,8 @@
 use clipboard_win::{Clipboard, formats, Getter, Setter, get_clipboard_string, set_clipboard_string};
 
 pub fn from_parameters(file: String, line: String) {
-    let mut link = String::from("codelink://") + &file + ":" + &line;
+    let mut str_parsed = file.replace("\\", "/");
+    let mut link = String::from("codelink://") + &str_parsed + ":" + &line;
     set_clipboard_string(&link).unwrap();
     println!("write clipboard {:?}", link);
     return;
